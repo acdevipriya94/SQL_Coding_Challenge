d885b29884ef45cc5c8a19813b792f54c315a8db
A C Devipriya <acdevipriya94@gmail.com>
2026-02-20 15:40:09 +0530

    Add files via upload

---
+++ SQL Coding Challenge_Task 1_DDL Commands.sql
@@ -0,0 +1,41 @@
+create database city_hospital;
+use city_hospital;
+
+create table Patients 
+(PatientID			 int, 
+ PatientName		 varchar(50),
+ Age				 int,
+ Gender				 enum('Male','Female'),
+ AdmissionDate		 date);
+
+alter table Patients
+add column DoctorAssigned varchar(50);
+
+select * from Patients;
+drop database city_hospital;
+create database city_hospital;
+use city_hospital;
+
+create table Patients 
+(PatientID			 int, 
+ PatientName		 varchar(50),
+ Age				 int,
+ Gender				 enum('Male','Female'),
+ AdmissionDate		 date);
+select * from Patients;
+
+alter table Patients
+add column DoctorAssigned varchar(50);
+
+alter table Patients
+modify column PatientName varchar(100);
+
+rename table Patients_Info to Patient_Info;
+
+truncate Patient_Info;
+
+select * from Patient_Info;
+
+drop table Patient_Info;
+
+
